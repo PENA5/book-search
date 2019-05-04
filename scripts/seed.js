cecb3323607e4ee3a03b5d2c70f07e4260410b50
PENA5 <44989115+PENA5@users.noreply.github.com>
2019-05-04 04:05:30 -0500

    server.js

---
+++ scripts/seed.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Book = require("../models/book");
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    family: 4 // Use IPv4, skip trying IPv6
+  }
+);
+
+const booksSeed = [
+  
+
+
+
+];
+
+async function seed() {
+  await mongoose
+    .connect(
+      MONGODB_URI,
+      options
+    )
+    .then(() => {
+      console.log("Seed: Connected to Database");
+    })
+    .catch(err => {
+      console.log("Seed: Not Connected to Database ERROR! ", err);
+    });
+  for (let book of booksSeed) {
+    const { _id: bookId } = await new Book({
+      title: book.title,
+      subtitle: book.subtitle,
+      authors: book.authors,
+      link: book.link,
+      description: book.description,
+      image: book.image,
+      googleId: book.googleId
+    }).save();
+  }
+
+  mongoose.disconnect();
+
+  console.info("Seed: Done!");
+}
+
+seed();
